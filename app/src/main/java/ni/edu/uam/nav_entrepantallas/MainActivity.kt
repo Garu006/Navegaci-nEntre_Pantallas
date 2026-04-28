a172c9d30e2a292a798806f6579b6d728a962a5f
Garu006 <garojas@uamv.edu.ni>
2026-04-27 20:16:25 -0600

    creación de simulación de pagina de gimnasio UAM con navegación entre pantallas utilizando jetpack Compose

--- app/src/main/java/ni/edu/uam/nav_entrepantallas/MainActivity.kt
+++ app/src/main/java/ni/edu/uam/nav_entrepantallas/MainActivity.kt
@@ -1,10 +1,10 @@
 package ni.edu.uam.nav_entrepantallas
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.mutableIntSetOf
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -25,6 +25,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ni.edu.uam.nav_entrepantallas.ui.theme.Nav_EntrePantallasTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.autofill.contentType
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,34 +53,236 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     AppNavigation(navController)
                 }
-                }
             }
         }
     }
 }
 
 @Composable
-fun AppNavigation(navController: NavHostController){
+fun AppNavigation(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = "Inicio"
-    ){
-        composable("Inicio"){
+        startDestination = "inicio"
+    ) {
+        composable("inicio") {
             PantallaInicio(navController)
         }
-        composable("detalle"){
+
+        composable("detalle") {
             PantallaDetalle(navController)
         }
-        composable("perfil"){
-            PantallaPerfil(nav)
+
+        composable("perfil") {
+            PantallaPerfil(navController)
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Nav_EntrePantallasTheme {
-        Greeting("Android")
+fun PantallaInicio(navController: NavHostController) {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "FitUAM",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.rutina_fuerza),
+                contentDescription = "Rutina de fuerza",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(220.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Rutinas personalizadas para mejorar tu condición fisica.")
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    navController.navigate("detalle")
+                }
+            ) {
+                Text("Ver ejercicio")
+            }
+        }
+    }
+}
+
+@Composable
+    fun PantallaDetalle(navController: NavHostController) {
+        val imagenes = listOf(
+            R.drawable.ejercicio_sentadilla,
+            R.drawable.rutina_fuerza
+        )
+        var imagenActual by remember {
+            mutableIntStateOf(0)
+        }
+        Scaffold { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(24.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = "Ejercicio: Sentadilla",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = imagenes[imagenActual]),
+                    contentDescription = "Imagen del ejercicio",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(220.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Instrucciones",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text("Series recomendadas: 4")
+                Text("Repeticiones: 12")
+                Text("Descanso: 60 segundos")
+
+                Button(
+                    onClick = {
+                        imagenActual = (imagenActual + 1) % imagenes.size
+                    }
+                ) {
+                    Text("Cambiar imagen")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        navController.navigate("perfil")
+                    }
+                ) {
+                    Text("Ver progreso")
+                }
+                Spacer(modifier = Modifier.height(12.dp))
+                Button(
+                    onClick = {
+                        navController.popBackStack()
+                    }
+                ){
+                    Text("Volver")
+                }
+            }
+        }
+}
+
+@Composable
+fun PantallaPerfil(navController: NavHostController) {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Perfil FitUAM",
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.perfil_gym),
+                contentDescription = "Perfil de entrenamiento",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(220.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "Resumen del usuario",
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text("Nombre: Gabriel")
+            Text("Nivel: Principiante")
+            Text("Rutinas completadas: 8")
+            Text("Objetivo: Mejorar fuerza y resistencia")
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "Progreso semanal: 70%",
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(18.dp)
+                    .background(
+                        color = Color.LightGray,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.7f)
+                        .height(18.dp)
+                        .background(
+                            color = Color(0xFF4CAF50),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    navController.navigate("inicio") {
+                        popUpTo("inicio") {
+                            inclusive = true
+                        }
+                    }
+                }
+            ) {
+                Text("Volver al Inicio")
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                onClick = {
+                    navController.popBackStack()
+                }
+            ) {
+                Text("Volver a Detalle")
+            }
+        }
     }
 }
